2b24a1d189947b5d480527d580d9c22f719100fc
kennethsim2000 <e0725254@u.nus.edu>
2023-05-09 18:28:53 +0800

    Add signing up functionality

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,49 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+import { compare } from "bcrypt";
+
+import prismadb from "@/lib/prismadb";
 
 export default NextAuth({
   providers: [
+    Credentials({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Email and password required");
+        }
+        const user = await prismadb.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (!user || !user.hashedPassword) {
+          throw new Error("Email does not exist");
+        }
+        const isCorrectPassword = await compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error("Incorrect password");
+        }
+
+        return user;
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID, //process.env.GOOGLE_CLIENT_ID
       clientSecret: process.env.GOOGLE_CLIENT_SECRET, //process.env.GOOGLE_CLIENT_SECRET
@@ -16,6 +57,12 @@ export default NextAuth({
       },
     }),
   ],
+  pages: {
+    signIn: "/",
+  },
   debug: true,
   secret: process.env.JWT_SECRET,
+  session: {
+    strategy: "jwt",
+  },
 });
