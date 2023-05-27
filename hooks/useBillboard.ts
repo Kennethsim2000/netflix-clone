f7cf07179d6c0ec4883d65bed04db94e2a6cc329
kennethsim2000 <e0725254@u.nus.edu>
2023-05-27 17:46:28 +0800

    Fix Movie Types

--- hooks/useBillboard.ts
+++ hooks/useBillboard.ts
@@ -1,8 +1,9 @@
 import useSWR from "swr";
 import fetcher from "@/lib/fetcher";
+import { Movie } from "@prisma/client";
 
 const useBillboard = () => {
-  const { data, error, isLoading } = useSWR<Movie[]>("/api/random", fetcher, {
+  const { data, error, isLoading } = useSWR<Movie>("/api/random", fetcher, {
     revalidateIfStale: false,
     revalidateOnFocus: false,
     revalidateOnReconnect: false,
